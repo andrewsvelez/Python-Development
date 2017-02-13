d3c49ae95b945b231a6b7b28e430546eabcc31c5
andrewsvelez <edanvelez@utp.edu.co>
2017-02-13 12:43:46 -0500

    creando pendiente de una recta en python

---
+++ pendienterecta.py
@@ -0,0 +1,10 @@
+#pendiente
+
+
+Xi = int(raw_input('Digite el valor de Xi: '))
+Yi = int(raw_input('Digite el valor de Yi: '))
+Xf = int(raw_input('Digite el valor de Xf: '))
+Yf = int(raw_input('Digite el valor de Yf: '))
+
+m= (Yf-Yi)/(Xf-Xi)
+print m
